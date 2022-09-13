9ff37bf2323dcf1b60e79adf3bcf5d4b1ce804c1
Wijayaac <wijayaac25@gmail.com>
2022-09-13 23:00:15 +0800

    Add : footer section

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import style from "./style.module.css";
+const Footer = () => {
+  return (
+    <footer className={style.footer}>
+      <div className={style.wrapper}>W3 Shools &copy; 2022</div>
+    </footer>
+  );
+};
+
+export default Footer;
